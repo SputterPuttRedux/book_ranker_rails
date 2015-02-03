165a65470901bcd309d0131eb16e53057432627e
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-02-02 21:16:02 -0500

    Rails-generated schema file

--- movie_ranker-rails/db/schema.rb
+++ movie_ranker-rails/db/schema.rb
@@ -11,9 +11,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150203002330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.integer "movie_id"
+    t.integer "genre_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string  "name"
+    t.text    "description"
+    t.integer "rating",      default: 0
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
