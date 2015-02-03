968e7ccb2c083c2aa46204e70c0de29853a82dfc
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-02-02 21:16:18 -0500

    Add more faker data to seed file

--- movie_ranker-rails/db/seeds.rb
+++ movie_ranker-rails/db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+dionne = User.create(
+  name: "dionne",
+  password: "cat"
+  )
+
+baby = Movie.create(
+  name: "Adventures in Babysitting",
+  description: "A lifetime of fun. In just one night",
+  rating: 5
+  )
+
+live_action = Genre.create(
+  name: "Live-Action"
+  )
+
+baby.genres << live_action
+dionne.movies << baby
+
+genres = ["Live-Action", "Foreign", "Comedy", "Drama", "Political","Science-Fiction", "Fantasy", "Historical"]
+
+10.times do |i|
+  Genre.create(
+    name: genres[i]
+    )
+end
+
+10.times do
+  Movie.create(
+    name: Faker::Lorem.word,
+    description: Faker::Lorem.paragraph,
+    rating: rand(1..5)
+    )
+end
+
+Movie.all.each do |mov|
+  mov.genres << Genre.all.sample(2)
+end
+
+10.times do |i|
+  User.create(
+    name: Faker::Name.name,
+    password: "cat"
+    )
+end
+
+User.all.each do |usr|
+  usr.movies << Movie.all.sample(rand(1..3))
+end
