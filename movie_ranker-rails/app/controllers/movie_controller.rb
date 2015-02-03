e9ce71ad679709a7d1c8fca0ad235e1fe2a45d14
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-02-02 22:14:11 -0500

    Add actions for new and create within Movie Controller Class

--- movie_ranker-rails/app/controllers/movie_controller.rb
+++ movie_ranker-rails/app/controllers/movie_controller.rb
@@ -2,5 +2,15 @@ class MovieController < ActionController::Base
   def show
     @movie = Movie.find_by(id: params[:id])
   end
+
+  def new
+
+  end
+
+  def create
+    Movie.create(params[:movie])
+  end
+
+
   protect_from_forgery with: :exception
 end
