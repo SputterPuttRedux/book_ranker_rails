42cb3e2922be3405beebf22ad35f137b370ccf00
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-02-02 21:15:10 -0500

    Remove extra 'create table' line of code

--- movie_ranker-rails/db/migrate/20150203002308_create_categories.rb
+++ movie_ranker-rails/db/migrate/20150203002308_create_categories.rb
@@ -1,7 +1,6 @@
 class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
-      create_table :categories do |t|
       t.references :movie
       t.references :genre
     end
