4485dc9fec45c5e0bf7721e23d6e9594cb644b51
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-02-02 21:57:17 -0500

    Create session_controller class w/ appropriate methods

---
+++ movie_ranker-rails/app/controllers/session_controller.rb
@@ -0,0 +1,27 @@
+class SessionController < ActionController::Base
+
+  def index
+  end
+
+  def create
+    user = User.new(params[:user])
+    session[:user_id] = user.id
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  protect_from_forgery with: :exception
+end
